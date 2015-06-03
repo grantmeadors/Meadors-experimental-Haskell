094c7b6ac4d0e49ca2ac444d6e618766d82f0498
Grant David Meadors <grant.meadors@ligo.org>
2015-06-03 17:06:11 +0200

    OK, starting to build something a bit more interesting

--- beginnings.hs
+++ beginnings.hs
@@ -3,7 +3,12 @@
 -- 02015-04-30 (JD 2457143)
 -- g r a n t . m e a d o r s @ a e i . m p g . d e
 
-main = putStrLn "This is a prelude to Haskell programming"
+--main = putStrLn "This is a prelude to Haskell programming"
+
+fac 0 = 1
+fac n = n * fac(n-1)
+--main = print (fac 42)
+
 --import Control.Parallel
 --main = completion
 
@@ -11,3 +16,17 @@ main = putStrLn "This is a prelude to Haskell programming"
 --    where
 --        a = putStrLn "This is a prelude to Haskell programming"
 --        b = putStrLn "And this is a continuation"
+
+--Can we design an elegant Fibonnaci program?
+
+fib 0 = 1
+fib 1 = 1
+fib n = fib(n-1) + fib(n-2)
+
+--main = do print (fib 3) 
+--main = do {putStrLn "Input a number" ; x <- readLn; putStrLn "Thanks"}
+
+
+
+main = do {putStrLn "Learning factorial and Fibonnaci values"; print ( fac x ); print ( fib x )}
+	where x=5
